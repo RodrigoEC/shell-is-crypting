cba3daa9d6211fd9bdc2f07d5ea825c263bd0509
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2021-12-28 22:42:55 -0300

    Adds alura version of this script

--- conversao_novos_livros.sh
+++ conversao_novos_livros.sh
@@ -3,18 +3,23 @@
 convert_files() {
     cd $1
 
-
     for file in *
     do
-        if [ -d $file ]
+        local file_path=$(find ~/Downloads/imagens-novos-livros -name $file)
+        if [ -d $file_path ]
         then
-            convert_files $1/$file
-            cd $1
+            convert_files $file_path
         else
-            local file_name=$(ls $file | awk -F. '{ print$1 }')
-            convert $file $file_name.png
+            local file_name=$(ls $file_path | awk -F. '{ print $1 }')
+            convert $file_name.jpg $file_name.png
         fi
     done
 }
 
-convert_files ~/Downloads/imagens-novos-livros 
+convert_files ~/Downloads/imagens-novos-livros
+if [ $? -eq 0 ]
+then
+    echo "Conversao realizada com sucesso!"
+else
+    echo "Houve algum problema na conversao"
+fi
