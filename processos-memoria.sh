cc3de3d0d2df623840af129d90a7574fdec60453
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2021-12-29 11:04:56 -0300

    update process-memoria.sh

--- processos-memoria.sh
+++ processos-memoria.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+if [ ! -d logs ]
+then
+    mkdir logs
+fi
+
 getMostExpensiveProcess() {
     local processes=$(ps -e -o pid --sort -size | head -n 11 | grep '[0-9]')
 
     for process in $processes
     do
-        echo $(ps -p $process -o comm=)
+        local process_name=$(ps -p $process -o comm=)
+        local current_date=$(date +%F,%H:%M:%S,)
+
+        echo -n $current_date >> logs/$process_name.log
+
+        local process_size=$(ps -p $process -o size | grep '[0-9]')
+        echo "$(bc <<< "scale=2;$process_size/1024") MB" >> logs/$process_name.log
+        
     done
 }
 
 getMostExpensiveProcess
+if [ $? -eq 0 ]
+then
+    echo "Processos analisados com sucesso!"
+else
+    echo "Não deu bom..."
+fi
